eb530a68527829ac992bfdbcaffa51971c1b240d
pedrokpp <pedrokp@id.uff.br>
2022-06-07 19:59:05 -0300

    definições na header para melhor organização da main

--- big_int.h
+++ big_int.h
@@ -8,7 +8,11 @@ typedef struct node {
     struct node* prev;
 } DIGIT;
 
-DIGIT* newDigit(int value);
+DIGIT* newDigit(int value) {
+    DIGIT* d = malloc(sizeof(DIGIT));
+    d->value = value;
+    return d;
+}
 
 typedef struct BIG_INT {
     int length;
@@ -16,6 +20,44 @@ typedef struct BIG_INT {
     DIGIT* tail;
 } BIG_INT;
 
-void append(BIG_INT* bi, int value);
-BIG_INT* parseString(char* number);
-void printNumber(BIG_INT* bi);
+void append(BIG_INT* bi, int value) {
+    DIGIT* d = newDigit(value);
+    if (bi->length == 0) {
+        d->next = d;
+        d->prev = d;
+        bi->head = d;
+        bi->tail = d;
+    } else {
+        DIGIT* tail = bi->tail;
+        tail->next = d;
+        d->prev = tail;
+        bi->tail = d;
+        d->next = bi->head;
+    }
+    bi->length++;
+}
+
+BIG_INT* parseString(char* number) {
+    BIG_INT* bi = malloc(sizeof(BIG_INT));
+    for (int i = 0; i < strlen(number); i++) {
+        if (!(number[i] >= '0' && number[i] <= '9'))
+            return NULL;
+        int num = number[i] - '0';
+        append(bi, num);
+    }
+    return bi;
+}
+
+void printNumber(BIG_INT* bi) {
+    DIGIT* current = bi->head;
+    if (!current) {
+        printf("Número vazio\n");
+        return;
+    }
+
+    do {
+       printf("%d", current->value); 
+       current = current->next;
+    } while (current != bi->head);
+    printf("\n");
+}
