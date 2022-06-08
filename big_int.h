435996825158c8ad8b0527dc8e5d8a97b883edd9
pedrokpp <pedrokp@id.uff.br>
2022-06-08 18:05:17 -0300

    add e sub feitos

--- big_int.h
+++ big_int.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 typedef struct node {
     int value;
@@ -29,11 +30,11 @@ void append(BIG_INT* bi, int value) {
         bi->head = d;
         bi->tail = d;
     } else {
-        DIGIT* tail = bi->tail;
-        tail->next = d;
-        d->prev = tail;
+        bi->tail->next = d;
+        d->prev = bi->tail;
         d->next = bi->head;
         bi->tail = d;
+        bi->head->prev = bi->tail;
     }
     bi->length++;
 }
@@ -47,11 +48,11 @@ void insert(BIG_INT* bi, int value) {
         bi->head = d;
         bi->tail = d;
     } else {
-        DIGIT* tail = bi->tail;
-        tail->next = d;
-        d->prev = tail;
+        bi->tail->next = d;
+        d->prev = bi->tail;
         d->next = bi->head;
         bi->head = d;
+        bi->head->prev = bi->tail;
     }
     bi->length++;
 }
@@ -79,4 +80,72 @@ void printNumber(BIG_INT* bi) {
        current = current->next;
     } while (current != bi->head);
     printf("\n");
+}
+
+BIG_INT* add(BIG_INT* bi1, BIG_INT* bi2) {
+    BIG_INT* res = malloc(sizeof(BIG_INT));
+    BIG_INT* bigger = bi1->length >= bi2->length ? bi1 : bi2;
+    BIG_INT* smaller = bi2->length <= bi1->length ? bi2 : bi1;
+
+    DIGIT* c_big = bigger->tail;
+    DIGIT* c_small = smaller->tail;
+    int carry = 0;
+
+    do {
+        if (!c_small) {
+            insert(res, c_big->value);
+        } else {
+            int result = c_big->value + c_small->value + carry;
+            carry = 0;
+            if (result >= 10) {
+                carry = floor(result / 10);
+                result = result % 10;
+            }
+            insert(res, result);
+        }
+        
+        if (c_small)
+            c_small = c_small->prev == smaller->tail ? NULL : c_small->prev;
+        c_big = c_big->prev;
+    } while(c_big != bigger->tail);
+
+    return res;
+}
+
+BIG_INT* sub(BIG_INT* bi1, BIG_INT* bi2) {
+    BIG_INT* res = malloc(sizeof(BIG_INT));
+    BIG_INT* bigger = bi1->length >= bi2->length ? bi1 : bi2;
+    BIG_INT* smaller = bi2->length <= bi1->length ? bi2 : bi1;
+
+    DIGIT* c_big = bigger->tail;
+    DIGIT* c_small = smaller->tail;
+    int carry = 0;
+
+    do {
+        if (!c_small) {
+            if (c_big->value > 0)
+                insert(res, c_big->value);
+        } else {
+            if (c_big->value < c_small->value) {
+                DIGIT* tmp = c_big->prev;
+                while (tmp != bigger->tail) {
+                    if (tmp->value > 0) {
+                        tmp->value--;
+                        break;
+                    }
+                    tmp->value = 9;
+                    tmp = tmp->prev;
+                }
+                insert(res, (10 + c_big->value) - c_small->value);
+            } else {
+                insert(res, c_big->value - c_small->value);
+            }
+        }
+        
+        if (c_small)
+            c_small = c_small->prev == smaller->tail ? NULL : c_small->prev;
+        c_big = c_big->prev;
+    } while(c_big != bigger->tail);
+
+    return res;
 }
