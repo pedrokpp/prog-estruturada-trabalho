aac85800cf3e1370b0a8c5a4e8c36512ce6961b8
fex0r <felipebelletti1@gmail.com>
2022-07-01 19:05:53 -0300

    C C feat: mult almost done

--- mult.h
+++ mult.h
@@ -95,6 +95,7 @@ void mult(BIG_INT* big1, BIG_INT* big2) {
     int iteratedLevel = 1; int iteratedLevel2 = 0;
     int casaLevel = 1;
     int casaLevel2 = 0;
+    res = parseString("");
 
     do {
         //printf("c1->value: %d |\n", current1->value);
@@ -102,38 +103,38 @@ void mult(BIG_INT* big1, BIG_INT* big2) {
         lowFactor = thisMultRes = current1->value * current2->value;
 
         if(upFactor > 0) {
+            printf("Usando o upFactor...\n");
             thisMultRes += upFactor;
             lowFactor += upFactor;
             upFactor = 0;
         }
 
         if(thisMultRes >= 10) {
-            upFactor = floor(thisMultRes / 10);
-            //lowfactor = current1 == big1->head ? thisMultRes : thisMultRes % 10;
-            lowFactor = thisMultRes % 10;
-            //printf("current: %d | big1->head: %d\n\n", current1, big1->head);
+            //lowFactor = thisMultRes % 10;
             if(current1 == big1->head) {
+                printf("Ja estamos no ultimo elemento, devo por TUDO\n");
                 lowFactor = thisMultRes;
             } else {
+                upFactor = floor(thisMultRes / 10);
                 lowFactor = thisMultRes % 10;
             }
         }
 
         resultCache[resultCacheIdx] = lowFactor * casaLevel * iteratedLevel; /* metodo antigo: nao aplicavel a big int's */
-        printf("ON-EXEC: _array[%d] = %d * %d * %d\n", resultCacheIdx, lowFactor, casaLevel, iteratedLevel);
+        //printf("ON-EXEC: _array[%d] = %d * %d * %d\n", resultCacheIdx, lowFactor, casaLevel, iteratedLevel);
 
-        printf(">> insert %d\n", lowFactor);
+        //printf(">> insert %d\n", lowFactor);
         insert(tempRes, lowFactor);
 
         for(int i = 0; i < iteratedLevel2; i++) {
-            printf(">> append 0, iteratedLevel2: %d\n", iteratedLevel2);
+            printf(">>?? append 0, iteratedLevel2: %d\n", iteratedLevel2);
             append(tempRes, 0);
         }
 
         casaLevel *= 10;
         resultCacheIdx++;
 
-        //printf("%d * %d = %d | lowFactor = %d | upFactor = %d | Inserindo %d na resposta\n", current1->value, current2->value, thisMultRes, lowFactor, upFactor, lowFactor);
+        printf("%d * %d = %d | lowFactor = %d | upFactor = %d | Inserindo %d na resposta\n", current1->value, current2->value, thisMultRes, lowFactor, upFactor, lowFactor);
         current1 = current1->prev;
         //printf("current1: %lx | big1->tail: %lx | current2: %lx | big2->tail: %lx | current2->prev: %lx\n", current1, big1->tail, current2, big2->tail, current2->prev);
         if( current1 == NULL && (current2 == NULL/* || current2->prev == NULL*/) ) {
@@ -142,35 +143,39 @@ void mult(BIG_INT* big1, BIG_INT* big2) {
         }
         if(current1 == NULL) { // iterou o elemento 1 inteiro
             for(int i = 0; i < casaLevel2; i++) {
-                printf(">> append 0, casaLevel2: %d\n", casaLevel2);
+                //printf(">> append 0, casaLevel2: %d\n", casaLevel2);
                 append(tempRes, 0);
             }
-            printf("***** ZEROING OUT CASALEVEL2\n");
+            //printf("***** ZEROING OUT CASALEVEL2\n");
             casaLevel = 1; casaLevel2 = -1; // -1 por resolucao de bugs
+            printf("+++++++ INC. ITERATEDLEVEL2\n");
             iteratedLevel *= 10; iteratedLevel2 += 1;
             current1 = big1->tail;
             current2 = current2->prev;
             if(current2 == NULL) {
-                printNumber(tempRes);
+                printf("SOMA FINAL\n");
                 res = soma(res, tempRes);
                 break;
             }
         }
 
-        printf("+++++ INC. CASALEVEL2\n");
+        //printf("+++++ INC. CASALEVEL2\n");
         casaLevel2 += 1;
 
         // RESET TEMPRES
-        printNumber(tempRes);
+        printf("OUT: res->tail: %p | tempRes->tail: %p\n", res->tail, tempRes->tail);
         res = soma(res, tempRes);
+
         memset(tempRes, 0, sizeof(BIG_INT));
+        //tempRes = parseString("");
 
     } while(1);
 
     //insert(res, intArraySum(resultCache, resultCacheIdx));
 
-    printf("ABAIXO:\n");
+    printf("RESULTADO BIGINT: ");
     printNumber(res);
+    printf("\n");
 
     printf("Final res: %d\n", intArraySum(resultCache, resultCacheIdx));
 }
--- soma.h
+++ soma.h
@@ -9,19 +9,11 @@ BIG_INT* soma(BIG_INT* bi1, BIG_INT* bi2) {
     DIGIT* c_big = bigger->tail;
     DIGIT* c_small = smaller->tail;
     int carry = 0;
-
-    if (bigger->head->value == 0) {
-        return smaller;
-    }
-    else if (smaller->head->value == 0) {
-        return bigger;
-    }
     
-
     do {
         if (!c_small) {
-            if (c_big->value != 0)
-                insert(res, c_big->value);
+            //if (c_big->value != 0)
+            insert(res, c_big->value);
         } else {
             int result = c_big->value + c_small->value + carry;
             carry = 0;
@@ -39,6 +31,25 @@ BIG_INT* soma(BIG_INT* bi1, BIG_INT* bi2) {
             c_small = c_small->prev;
         c_big = c_big->prev;
     } while(c_big);
+        
+
+    printNumber(bigger);
+    printf("+\n");
+    printNumber(smaller);
+    printf("---------\n");
+    printNumber(res);
+    printf("bigger length: %d\n", bigger->length);
+    printf("smaller length: %d\n", bigger->length);
+    printf("res length: %d\n", res->length);
+    printf("\n\n");
+
+    /* // debug
+    if(bi1->tail == NULL) {
+        printf("SOMA: %d + NULL! = | bi1->tail address: %p | bi2->tail address: %p\n", bi2->tail->value, bi1->tail, bi2->tail);
+    } else {
+        printf("SOMA: %d + %d = | bi1->tail address: %p | bi2->tail address: %p\n", bi1->tail->value, bi2->tail->value, bi1->tail, bi2->tail);
+    }
+    */
 
     return res;
 }
