bd0508a828156f33c4456147dee518c351ae25c0
pedrokpp <pedrokp@id.uff.br>
2022-07-03 15:34:36 -0300

    fix soma 2

--- soma.h
+++ soma.h
@@ -23,8 +23,7 @@ BIG_INT* soma(BIG_INT* bi1, BIG_INT* bi2) {
                 if (!c_big->prev && carry != 0)
                     insert(res, carry);
             } else {
-                if (c_big->value != 0)
-                    insert(res, c_big->value);
+                insert(res, c_big->value);
             }
         } else {
             int result = c_big->value + c_small->value + carry;
@@ -42,29 +41,6 @@ BIG_INT* soma(BIG_INT* bi1, BIG_INT* bi2) {
             c_small = c_small->prev;
         c_big = c_big->prev;
     } while(c_big);
-        
-
-    /*
-    printNumber(bigger);
-    printf("+\n");
-    printNumber(smaller);
-    printf("---------\n");
-    printNumber(res);
-    printf("bigger length: %d\n", bigger->length);
-    printf("smaller length: %d\n", smaller->length);
-    printf("res length: %d\n", res->length);
-    //printf("bigger addr: %p\n", bigger);
-    //printf("smaller addr: %p\n", smaller);
-    printf("\n\n");
-    */
-
-    /* // debug
-    if(bi1->tail == NULL) {
-        printf("SOMA: %d + NULL! = | bi1->tail address: %p | bi2->tail address: %p\n", bi2->tail->value, bi1->tail, bi2->tail);
-    } else {
-        printf("SOMA: %d + %d = | bi1->tail address: %p | bi2->tail address: %p\n", bi1->tail->value, bi2->tail->value, bi1->tail, bi2->tail);
-    }
-    */
 
     return res;
 }
